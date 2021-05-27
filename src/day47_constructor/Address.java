9cf3f229696424647c5d0b7215659324bacf7151
AncaNemes <lia03ro@yahoo.com>
2021-05-27 18:56:54 -0500

    first commit

---
+++ src/day47_constructor/Address.java
@@ -0,0 +1,66 @@
+package day47_constructor;
+
+public class Address {
+
+    //Encapsulate -private data in the address:
+    //street
+    //city
+    //state
+    //zipcode
+    //country
+
+private String street;
+private String city;
+private String state;
+private String zipcode;
+private String country = "USA";
+
+//generate : getters/setters
+// generate :toString
+//   "123 JAva St Apt 101, Selenium, Va, 22102"
+
+@Override
+public String toString(){
+    return street + ", "+ city+state+ "" + zipcode;
+}
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
