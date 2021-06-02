a95a05decca9faba7ea4d6e9feabc8c751c4fd9f
AncaNemes <lia03ro@yahoo.com>
2021-06-01 20:06:06 -0500

    commint from day49static keyword intro

--- src/day49_static/StaticMethods.java
+++ src/day49_static/StaticMethods.java
@@ -1,4 +1,39 @@
 package day49_static;
 
 public class StaticMethods {
+
+   int num=10;
+   static int count= 5;
+
+    public static void displayMessage(String message){
+
+
+        System.out.println("message" +message);
+
+      //  System.out.println("num " +num); num is not static
+        System.out.println("count = " +  count);// one static can use another static variable
+
+
+
+
+
+    }
+
+    public static void anotherStaticMethod(){
+        System.out.println("another static method" );
+        displayMessage("wooden spoon");
+
+    }
+    //StaticMethod.instanceMethod(); > no
+    //StaticMethod stm =new StaticMethod();
+    //stm.instanceMethod();
+
+    public void instanceMethod(){
+        System.out.println("instanceMethod");
+        System.out.println("num= " +num);
+        System.out.println("count= " +count);
+        System.out.println();
+        displayMessage("woodenspoon");
+    }
 }
+
