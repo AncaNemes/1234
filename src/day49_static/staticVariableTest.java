4042d0487e78b48d5b25eaf5bff759adabae706b
AncaNemes <lia03ro@yahoo.com>
2021-06-01 21:11:06 -0500

    2nd commit

---
+++ src/day49_static/staticVariableTest.java
@@ -0,0 +1,14 @@
+package day49_static;
+
+public class staticVariableTest {
+    public static void main(String[] args) {
+        System.out.println(StaticVariables.name);
+        System.out.println(StaticVariables.userName);
+        System.out.println(StaticVariables.age);
+        System.out.println(StaticVariables.price);
+      //  System.out.println(driver.findElement(By.xpath("+Locators.userName +").sendKeys("+")));
+        System.out.println(Locators.password);
+            System.out.println(Integer.MAX_VALUE);
+
+    }
+}
